acefb9957d18492e38648fbccfee68bf72bf9740
Marius Golabski <mgolabski@gmail.com>
2023-07-06 10:39:37 +0200

    add show answer button functionality

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const cardAnswer = document.querySelector('[data-js="card-answer"]');
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--active");
 });
