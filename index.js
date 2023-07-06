a7ea9e1761b8069647bd3dcdc85d9f28592e03fa
Marius Golabski <mgolabski@gmail.com>
2023-07-06 10:28:28 +0200

    delete log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 
 bookmarkButton.addEventListener("click", () => {
-  console.log("click");
   bookmarkButton.classList.toggle("bookmark--active");
 });
